a9a19fb4c32a9a2b6e63668d100331e37e6d90ac
prashant8khatiwada <prashantkhatiwada554@gmail.com>
2022-10-16 18:48:09 +0545

    finally

---
+++ 09_dadJokes/script.js
@@ -0,0 +1,16 @@
+const jokeEl = document.getElementById("jokes");
+const btn = document.querySelector(".btn");
+
+generateJoke();
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+  jokeEl.innerText = data.joke;
+}
